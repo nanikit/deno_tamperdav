9618d8a42e7d4c3f0d0ff50ac37192548e0f4b30
nanikit <nanikit4@gmail.com>
2023-10-15 22:56:49 +0900

    refactor: separate dependencies

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+export { type Deferred, deferred } from "https://deno.land/std@0.203.0/async/deferred.ts";
+export { join, normalize, resolve } from "https://deno.land/std@0.203.0/path/posix.ts";
+export { open } from "https://deno.land/x/open@v0.0.6/index.ts";
+export { default as chokidar } from "npm:chokidar";
+export { default as dialog } from "npm:dialog";
