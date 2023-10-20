7d689181a670ad7fc14eaf7442c5a11c8156b9e7
nanikit <nanikit4@gmail.com>
2023-10-21 03:07:40 +0900

    refactor: move top-level statements to main

--- src/dav_server.ts
+++ src/dav_server.ts
@@ -1,97 +1,8 @@
 import { chokidar, Deferred, deferred, join, normalize, open, resolve } from "./deps.ts";
 
 let args = {} as Record<string, unknown>;
+let working_dir = "";
 
-// early parsing for the config option
-const optionConfig = Deno.args.reduce(
-  (a, c) => c.split("--config=")[1] || a,
-  "",
-);
-const config = optionConfig || "config.json";
-
-try {
-  args = JSON.parse(await Deno.readTextFile(config));
-} catch (err) {
-  if (!(err instanceof Deno.errors.NotFound)) {
-    console.error(`${config}: ${err.message}`);
-    Deno.exit(1);
-  }
-}
-
-for (const k of ["username", "password"]) {
-  args[k] = args[k] || Deno.env.get("TD_" + k.toUpperCase());
-}
-
-for (const val of Deno.args) {
-  const s = val.replace(/^[-]{1,2}/, "").split("=");
-  args[s[0]] = s[1] || true;
-}
-
-if (Deno.args.includes("--help")) {
-  const { os } = Deno.build;
-  console.log(
-    `Usage: ${os === "windows" ? "TamperDAV.bat" : "./tamperdav.sh"} [options]
-Starts a WebDAV server using Node.js
-
-Options:
-        --help                     Shows this information
-        --config=[path]            Uses the specified config file (default: config.json)
-        --no-auth-warning          Disables the warning regarding missing authentication due
-                                   to missing username and password
-        --meta-touch               Updates corresponding meta file upon changing a script
-                                   file, resulting in connected browsers syncing these changes
-        --debug                    Provides some more detailed output for debugging purposes
-        --open-in-editor=[editor]  The editor to use when pressing the cloud editor icon in
-                                   Tampermonkey
-        --open-in-editor           Same as above, but uses ${
-      os === "windows" ? "notepad.exe" : "the editor chosen by xdg-open"
-    }
-        --username=[username]      The username for clients to authenticate to the server with
-        --password=[password]      The password for clients to authenticate to the server with
-        --host=[host]              The network address to bind on (default: localhost)
-        --port=[port]              The port that the server will listen on (default: 7000)
-        --path=[path]              The path, relativePath to server.js, that will serve as storage
-
-All of these options except "--help" can be specified in a JSON formatted file config.json
-in the same directory as server.js. An example is:
-{
-    "path": "dav",
-    "meta-touch": true,
-    "username": "foo",
-    "password": "bar",
-    "port": 1234
-}
-
-Options provided as command line parameters all have precedence over options stored in the
-config file.
-
-Username and password can also be specified in the environment variables TD_USERNAME and
-TD_PASSWORD respectively. These have priority over over the config file, but not over the
-command line parameters.
-`,
-  );
-  Deno.exit(0);
-}
-
-if (!args.path) {
-  console.error("path arguments missing");
-  Deno.exit(1);
-}
-
-const working_dir = join(".", args.path as string);
-await Deno.mkdir(working_dir, { recursive: true });
-
-if (!args["no-auth-warning"] && (!args.username || !args.password)) {
-  console.warn(
-    "TamperDAV is running without any form of authentication. It's strongly recommended to configure username and password!",
-  );
-}
-
-function regexEscape(s: string) {
-  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-}
-
-const port = args.port || 7000;
 const subscribers = {} as Record<string, Record<string, Deferred<Response>>>;
 
 const methods = {
@@ -434,11 +345,55 @@ function assureWatcher(relativePath: string) {
   }
 }
 
-export function main() {
-  Deno.serve(
+export async function main() {
+  // early parsing for the config option
+  const optionConfig = Deno.args.reduce(
+    (a, c) => c.split("--config=")[1] || a,
+    "",
+  );
+  const config = optionConfig || "config.json";
+
+  try {
+    args = JSON.parse(await Deno.readTextFile(config));
+  } catch (err) {
+    if (!(err instanceof Deno.errors.NotFound)) {
+      console.error(`${config}: ${err.message}`);
+      Deno.exit(1);
+    }
+  }
+
+  for (const k of ["username", "password"]) {
+    args[k] = args[k] || Deno.env.get("TD_" + k.toUpperCase());
+  }
+
+  for (const val of Deno.args) {
+    const s = val.replace(/^[-]{1,2}/, "").split("=");
+    args[s[0]] = s[1] || true;
+  }
+
+  if (Deno.args.includes("--help")) {
+    printHelp();
+  }
+
+  if (!args.path) {
+    console.error("path arguments missing");
+    Deno.exit(1);
+  }
+
+  working_dir = join(".", args.path as string);
+  await Deno.mkdir(working_dir, { recursive: true });
+
+  if (!args["no-auth-warning"] && (!args.username || !args.password)) {
+    console.warn(
+      "TamperDAV is running without any form of authentication. It's strongly recommended to configure username and password!",
+    );
+  }
+
+  const port = Number(args.port || 7000);
+  const server = Deno.serve(
     {
       hostname: (args.host as string) || "localhost",
-      port: port as number,
+      port: port,
       onError: (error) => {
         console.error(error);
         return new Response(`${error}`);
@@ -447,6 +402,53 @@ export function main() {
     logAndHandleRequest,
   );
   console.log(`server is listening on ${port}`);
+  return server;
+}
+
+function printHelp() {
+  const { os } = Deno.build;
+  console.log(
+    `Usage: ${os === "windows" ? "TamperDAV.bat" : "./tamperdav.sh"} [options]
+Starts a WebDAV server for Tampermonkey to sync scripts with.
+
+Options:
+        --help                     Shows this information
+        --config=[path]            Uses the specified config file (default: config.json)
+        --no-auth-warning          Disables the warning regarding missing authentication due
+                                   to missing username and password
+        --meta-touch               Updates corresponding meta file upon changing a script
+                                   file, resulting in connected browsers syncing these changes
+        --debug                    Provides some more detailed output for debugging purposes
+        --open-in-editor=[editor]  The editor to use when pressing the cloud editor icon in
+                                   Tampermonkey
+        --open-in-editor           Same as above, but uses ${
+      os === "windows" ? "notepad.exe" : "the editor chosen by xdg-open"
+    }
+        --username=[username]      The username for clients to authenticate to the server with
+        --password=[password]      The password for clients to authenticate to the server with
+        --host=[host]              The network address to bind on (default: localhost)
+        --port=[port]              The port that the server will listen on (default: 7000)
+        --path=[path]              The path, relativePath to server.js, that will serve as storage
+
+All of these options except "--help" can be specified in a JSON formatted file config.json
+in the same directory as server.js. An example is:
+{
+    "path": "dav",
+    "meta-touch": true,
+    "username": "foo",
+    "password": "bar",
+    "port": 1234
+}
+
+Options provided as command line parameters all have precedence over options stored in the
+config file.
+
+Username and password can also be specified in the environment variables TD_USERNAME and
+TD_PASSWORD respectively. These have priority over over the config file, but not over the
+command line parameters.
+`,
+  );
+  Deno.exit(0);
 }
 
 async function logAndHandleRequest(request: Request): Promise<Response> {
@@ -599,4 +601,8 @@ async function arrayToXml(
       })
       .join("\n");
   }
+}
+
+function regexEscape(s: string) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 }
